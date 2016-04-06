dc13eed3588c6fd7769090b4f2a0d0274aa61f09
mihkel Lukats <mihke.lukats@gmail.com>
2016-04-06 17:48:20 +0300

    fix

--- merge_lists.java
+++ merge_lists.java
@@ -5,19 +5,19 @@ import java.util.stream.Collectors;
 
 
 public class Main {
-
     public static void main(String[] args) {
         List<String> arrayList = new ArrayList<>(Arrays.asList("1", "2"));
         List<String> newList = Arrays.asList("3","4");
         
-        arrayList.stream()
+        newList.stream()
             .map(s -> doSomething(s))
             .collect(Collectors.toCollection(() -> arrayList));
+        
+        System.out.println(arrayList);
     }
     
     public static String doSomething(String s) {
         return s;
     }
-
 }
 
