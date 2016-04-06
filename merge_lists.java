8f1852adac2c8e9295a2389ee84ff35d55acd24f
mihkel Lukats <mihke.lukats@gmail.com>
2016-04-06 17:44:16 +0300

    stream example for lauri

---
+++ merge_lists.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>(Arrays.asList("1", "2"));
+        List<String> newList = Arrays.asList("3","4");
+        
+        arrayList.stream()
+            .map(s -> doSomething(s))
+            .collect(Collectors.toCollection(() -> arrayList));
+    }
+    
+    public static String doSomething(String s) {
+        return s;
+    }
+
+}
+
